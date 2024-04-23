95c5a90179ee75b5aa02c959b955f4b82dc9fdb6
Brad Madigan <bradmadigan@gmail.com>
2024-04-23 17:48:31 -0400

    WIP

--- resources/views/livewire/pages/customers/customer-list.blade.php
+++ resources/views/livewire/pages/customers/customer-list.blade.php
@@ -10,7 +10,7 @@
         </div>
     </x-slot>
 
-    <div class="flow-root"
+    <div class="flow-root">
         <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="inline-block min-w-full py-4 align-middle sm:px-6 lg:px-8">
 
